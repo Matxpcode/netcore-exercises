87cf178c5e03b9f0d11b0408963e89af2238ff3d
Matxpcode <mateo.rubiuz@gmail.com>
2026-07-08 05:30:21 -0500

    feat: agregar archivo de ejercicios http

---
+++ seccion1_http/MyFirstApp/MyFirstApp/Program.cs
@@ -0,0 +1,125 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+//app.MapGet("/", () => "Hello World!");
+
+app.Run(async (HttpContext context) =>
+{
+    //Ejercicio 1 : Modificacion del estado de Response
+    //if (1 == 1)
+    //{
+    //    context.Response.StatusCode = 200;
+    //}
+    //else
+    //{
+    //    context.Response.StatusCode=400;
+    //}
+
+    //Ejercicio 2 : Manejo y asignacion de Claves
+    //string path = context.Request.Path;
+    //string method = context.Request.Method;
+
+    //context.Response.Headers["MyKey"] = "my value";
+    //context.Response.Headers["Server"] = "My server";
+    //context.Response.Headers["Content-Type"] = "text/html";
+
+    //await context.Response.WriteAsync("<h1>Hello</h1>");
+    //await context.Response.WriteAsync("<h2>World</h2>");
+    //await context.Response.WriteAsync($"<p>{path}</p>");
+    //await context.Response.WriteAsync($"<p>{method}</p>");
+
+
+    //Ejercicio 3: Validacion del tipo de metodo y manejo de parametros con Query
+    //context.Response.Headers["Content-type"] = "text/html";
+    //if (context.Request.Method == "GET")
+    //{
+    //    if (context.Request.Query.ContainsKey("id"))
+    //    {
+    //        string id = context.Request.Query["id"];
+    //        await context.Response.WriteAsync($"<p>{id}</p>");
+    //    }
+    //}
+
+    //Ejercicio 4: Verificacion del tipo de navegador con User-Agent
+    //context.Response.Headers["Content-type"] = "text/html";
+    //if (context.Request.Headers.ContainsKey("User-Agent"))
+    //{
+    //    string userAgent = context.Request.Headers["User-Agent"];
+    //    await context.Response.WriteAsync($"<p>{userAgent}</p>");
+    //}
+
+
+    // CASOS ADICIONALES : Manejo de Path personalizados
+
+    //CASO 1
+    //context.Response.Headers["Content-Type"] = "text/html; charset=utf-8";
+    //string path = context.Request.Path.ToString().ToLower();
+
+    //if (path == "/")
+    //{
+    //    //Usamos boostrap integrado mediante CDN en el HTML
+    //    await context.Response.WriteAsync(@"
+    //        <link href=""https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css"" rel=""stylesheet"" integrity=""sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB"" crossorigin=""anonymous"">
+    //        <div class = 'container mt-5'>
+    //            <div class = 'alert alert-primary text-center'>
+    //                <h1>!Bienvenido a mi Servidor Core¡</h1>
+    //                <p>Estas en la pagina de inicio corporativa.</p>
+    //                <a href='/contacto' class='btn btn-outline-primary'>Ir a Contacto</a>
+    //            </div>
+    //        </div>
+    //    ");
+    //}
+    //else if (path == "/contacto")
+    //{
+    //    await context.Response.WriteAsync(@"
+    //        <link href=""https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css"" rel=""stylesheet"" integrity=""sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB"" crossorigin=""anonymous"">
+    //        <div class = 'container mt-5'>
+    //           <h1>!Contacto de Soporte¡</h1>
+    //           <p>Escribenos a: soporte@cibertec.edu.pe</p>
+    //           <a href='/' class='btn btn-secondary'>Regresar</a>
+    //        </div>
+    //    ");
+    //}
+    //else
+    //{
+    //    //Si escribe cualquier otra cosa, forzamos el codigo 404 en el Network
+    //    context.Response.StatusCode = 404;
+    //    await context.Response.WriteAsync(@"
+    //        <link href=""https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css"" rel=""stylesheet"" integrity=""sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB"" crossorigin=""anonymous"">
+    //        <h1 class='text-danger text-center mt-5'>404 - Pagina no encontrada</h1>
+    //    ");
+    //}
+
+    //CASO 2
+    context.Response.Headers["Content-Type"] = "text/html; charset=utf-8";
+    if (context.Request.Query.ContainsKey("nombre") && context.Request.Query.ContainsKey("nota"))
+    {
+        string alumno = context.Request.Query["nombre"];
+        int nota = int.Parse(context.Request.Query["nota"]);
+
+        string claseBoostrap = nota >= 13 ? "alert-success" : "alert-danger";
+        string resultado = nota >= 13 ? "APROBADO" : "DESAPROBADO";
+
+        await context.Response.WriteAsync($@"
+        <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB' crossorigin='anonymous'>
+
+        <div class='container mt-5'>
+            <div class='alert {claseBoostrap}'>
+                <h3>Alumno: {alumno}</h3>
+                <p>Nota final: <strong>{nota}</strong></p>
+                <hr>
+                <h4>Estado: {resultado}</h4>
+            </div>
+        </div>
+        ");
+    }
+    else
+    {
+        await context.Response.WriteAsync(@"
+            <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css' rel='stylesheet'>
+            <p>Por favor envia los parametros: <code>?nombre=Mateo&amp;nota=15</code> en la URL</p>");
+    }
+
+});
+
+app.Run();
